90a58ac47603003c4424f92fdfce5fc0c55576ff
F-4Dev <fabian.imgrund2@gmail.com>
2026-05-07 22:33:09 +0200

    update

--- scripts/inst-all-script.sh
+++ scripts/inst-all-script.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 echo "welcome back :)"
 
-echo "\\n[installing prequesits  ...]"
+echo "[installing prequesits  ...]"
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && \
 
-echo "\\n[installing dank linux]"
+echo "[installing dank linux]"
 curl -fsSL https://install.danklinux.com | sh
 
-echo "\\n[cloning repo ...]"
+echo "[cloning repo ...]"
 git clone https://github.com/F-4Dev/lapdots.git tmp && cd tmp
 
-echo "\\n[installing packages ...]"
+echo "[installing packages ...]"
 yay --noconfirm -S  $(cat packages)
 
-echo "\\n[zsh ...]"
+echo "[zsh ...]"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s $(which zsh)
 
-echo "\\n[enabling hyprexpo ...]"
+echo "[enabling hyprexpo ...]"
 hyprpm add https://github.com/hyprwm/hyprland-plugins
 hyprpm enable hyprexpo
 hyprpm reload
 
-echo "\\n[moving files ...]"
+echo "[moving files ...]"
 cp hyprland.conf ~/.config/hypr/hyprland.conf
 cp dms/binds.conf ~/.config/hypr/dms/binds.conf
 cp .zshrc ~/.zshrc
@@ -30,7 +30,7 @@ cp .gitignore ~/.config/hypr/.gitignore
 cp VSCodium/* .config/VSCodium/User/
 mkdir -p Pictures/wallpaper && mv wallpaper/* Pictures/wallpaper/
 
-echo "\\n[system will now reboot, until we meet again :) ]"
+echo "[system will now reboot, until we meet again :) ]"
 sleep 1
 echo "1\
 sleep 1
